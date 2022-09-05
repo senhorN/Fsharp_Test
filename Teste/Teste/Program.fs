de76b151dac4727aaa563c1f46563b855b1288ed
Nicolas pereira <90424448+senhorN@users.noreply.github.com>
2022-09-05 12:02:11 -0300

    Add files via upload

---
+++ Teste/Teste/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let f () = 
+    printf"Hello World!"//olá mundo para f#
+
+[<EntryPoint>]
+let main argv =
+    
+    let main argv=
+        f()
+    0// return an integer exit code
